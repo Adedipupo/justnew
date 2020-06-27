90812fdf8c2a49eb9bdab3eb17aa4ae3769fe087
Adedipupo <bayonleolaluwa@gmail.com>
2020-06-27 02:06:33 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React, { Component } from "react"
 
+/**
+ * Challenge: Wire up the partially-finished travel form so that it works!
+ * Remember to use the concept of controlled forms
+ * https://reactjs.org/docs/forms.html
+ * 
+ * All information should be populating the text below the form in real-time
+ * as you're filling it out
+ * 
+ * This exercise is adapted from the V School curriculum on vanilla JS forms:
+ * https://coursework.vschool.io/travel-form/
+ * 
+ * All of our challenges and learning resources are open for the public
+ * to play around with and learn from at https://coursework.vschool.io
+ */
 
 class App extends Component {
   constructor() {
@@ -10,51 +24,51 @@ class App extends Component {
       age: "",
       gender: "",
       destination: "",
-      dietaryRestrictions: {
-        isVegan: false,
-        isKosher: false,
-        isLactoseFree: false
-      }
-
+      isVegan: false,
+      isKosher: false,
+      isLactoseFree: false
     }
     this.handleChange = this.handleChange.bind(this)
   }
- 
+
   handleChange(event) {
-    const{ name , value} = event.target
-    this.setState({
-      [name] : value
-    })
+    const { name, value, type, checked } = event.target
+    type === "checkbox" ?
+      this.setState({
+        [name]: checked
+      })
+      :
+      this.setState({
+        [name]: value
+      })
   }
 
   render() {
     return (
       <main>
         <form>
-          <input 
-            name="firstName" 
-            value={this.state.firstName} 
-            onChange={this.handleChange} 
-            placeholder="First Name" 
-            
+          <input
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+            placeholder="First Name"
           />
           <br />
 
-          <input 
+          <input
             name="lastName"
             value={this.state.lastName}
             onChange={this.handleChange}
-            placeholder="last Name" 
+            placeholder="Last Name"
           />
-
           <br />
 
-          <input name="age"
+          <input
+            name="age"
             value={this.state.age}
             onChange={this.handleChange}
-            placeholder="age" 
-          /> 
-
+            placeholder="Age"
+          />
           <br />
 
           <label>
@@ -65,10 +79,10 @@ class App extends Component {
               checked={this.state.gender === "male"}
               onChange={this.handleChange}
             /> Male
-          </label>
+                    </label>
 
           <br />
-           
+
           <label>
             <input
               type="radio"
@@ -77,7 +91,7 @@ class App extends Component {
               checked={this.state.gender === "female"}
               onChange={this.handleChange}
             /> Female
-          </label>
+                    </label>
 
           <br />
 
@@ -92,6 +106,7 @@ class App extends Component {
             <option value="north pole">North Pole</option>
             <option value="south pole">South Pole</option>
           </select>
+
           <br />
 
           <label>
@@ -99,7 +114,7 @@ class App extends Component {
               type="checkbox"
               name="isVegan"
               onChange={this.handleChange}
-              checked={this.state.dietaryRestrictions.isVegan}
+              checked={this.state.isVegan}
             /> Vegan?
                     </label>
           <br />
@@ -109,19 +124,19 @@ class App extends Component {
               type="checkbox"
               name="isKosher"
               onChange={this.handleChange}
-              checked={this.state.dietaryRestrictions.isKosher}
+              checked={this.state.isKosher}
             /> Kosher?
-          </label>
+                    </label>
           <br />
+
           <label>
             <input
               type="checkbox"
               name="isLactoseFree"
               onChange={this.handleChange}
-              checked={this.state.dietaryRestrictions.isLactoseFree}
+              checked={this.state.isLactoseFree}
             /> Lactose Free?
-          </label>
-
+                    </label>
           <br />
 
           <button>Submit</button>
@@ -129,13 +144,15 @@ class App extends Component {
         <hr />
         <h2>Entered information:</h2>
         <p>Your name: {this.state.firstName} {this.state.lastName}</p>
-        <p>Your age: {/* Age here */}</p>
+        <p>Your age: {this.state.age}</p>
         <p>Your gender: {this.state.gender}</p>
         <p>Your destination: {this.state.destination}</p>
-        <p>
-          Your dietary restrictions:
-                    {/* Dietary restrictions here, comma separated */}
-        </p>
+        <p>Your dietary restrictions:</p>
+
+        <p>Vegan: {this.state.isVegan ? "Yes" : "No"}</p>
+        <p>Kosher: {this.state.isKosher ? "Yes" : "No"}</p>
+        <p>Lactose Free: {this.state.isLactoseFree ? "Yes" : "No"}</p>
+
       </main>
     )
   }
